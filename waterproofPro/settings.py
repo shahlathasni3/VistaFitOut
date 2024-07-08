de5d5006b4170059de4d400bacaba04b28169ea9
Shahla Thasni <shahlathasni3@gmail.com>
2024-07-08 19:11:29 +0530

    Add store migrations

--- waterproofPro/settings.py
+++ waterproofPro/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # "whitenoise.middleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "waterproofPro.urls"
@@ -79,13 +79,16 @@ WSGI_APPLICATION = "waterproofPro.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+    'default': dj_database_url.config(os.environ.get('DATABASE_URL'))
 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- waterproofPro/urls.py
+++ waterproofPro/urls.py
@@ -20,6 +20,7 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -27,3 +28,4 @@ urlpatterns = [
     path("",views.home, name='home'),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
+urlpatterns += staticfiles_urlpatterns()
